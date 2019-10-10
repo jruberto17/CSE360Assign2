360c12c3d3f77b6019311d4f20f56058288479da
James Ruberto <jruberto@asu.edu>
2019-10-09 21:04:36 -0700

    Initial commit.

---
+++ 360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,38 @@
+/**
+ * @author James Ruberto
+ * Class ID: CSE 360
+ * Assignment 2
+ * This assignment is a basic adding machine that allows us to practice using a version control system
+*/
+
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public String toString () {
+		return "";
+	}
+	
+	public void clear() {
+		
+	}
+	
+}
